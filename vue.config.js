09593e7a7609022d528adf3ae6e7c7bd1ce538fb
yaobaozai <282926102@qq.com>
2018-11-23 22:35:03 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'common': resolve('src/common'),
+        'components': resolve('src/components')
+      }
+    }
+  }
+}
